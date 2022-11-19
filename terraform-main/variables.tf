5bdf7e15ba8ca17f9d39431a50d17999a1053f0f
leomozzer <mozzer.ferraz@gmail.com>
2022-11-19 13:11:57 -0300

    Added stage variable

--- terraform-main/variables.tf
+++ terraform-main/variables.tf
@@ -5,3 +5,7 @@ variable "rg_location" {
 variable "repo_url" {
   description = "GitHub repository where the app is located"
 }
+
+variable "stage" {
+  description = "Stage that the app will be deployed (dev, prod, test)"
+}
